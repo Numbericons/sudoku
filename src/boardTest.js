cf3775cb73a8d49514e634f662c69b49b5b2471b
Zachary <zachjoliver@gmail.com>
2020-06-08 00:19:42 -0700

    Change console log adjust get col to ask for specific element adjust triedin sample next adjust get priority valid next function calls and adjust check valid

--- src/boardTest.js
+++ src/boardTest.js
@@ -41,10 +41,13 @@ function common(cols,row) {
   return false;
 }
 
-function chkValid(arr, len, n, i, j) {
+function chkValid(arr, numbers, n, i, j) {
   // const idx = arr.length - 1 + n;
-  const col = getCol(arr, len, n);
-  const row = getRow(arr, len, i, j, offset = 0)
+  debugger;
+  const number = numbers[n-1];
+  const col = getCol(arr, numbers.length, n);
+  const row = getRow(arr, numbers.length, i, j+1);
+  return !col.includes(number) && !row.includes(number);
 }
 // need to get row and columns for both elements
 
@@ -52,8 +55,8 @@ function chkValid(arr, len, n, i, j) {
 function getPriority(numbers, nextRow, arr, i, j) {
   debugger;
   if (arr.length === 1) return splPriority(numbers, nextRow, arr[0]);
-  const validNext = chkValid(arr, numbers.length, 1, i, j);
-  const validNext2 = chkValid(arr, numbers.length, 2, i, j);
+  const validNext = chkValid(arr, numbers, 1, i, j);
+  const validNext2 = chkValid(arr, numbers, 2, i, j);
   if (validNext && validNext2) {
     return Math.random > .5 ? splPriority(numbers, nextRow, arr[0]) : splPriority(numbers, nextRow, arr[1]);
   } else if (validNext) {
@@ -91,6 +94,7 @@ function sampleNext(numbers, nRow, nextCols, row, nTried, i, j){
     //   let idx = randIdx(nRow);
     //   el = nRow.splice(idx,1)[0];
     // }
+    if (!nTried.includes(el)) nTried.push(el);
     const numbIdx = numbers.indexOf(el);
     return numbers.splice(numbIdx, 1)[0];
   } else {
@@ -108,7 +112,7 @@ function getStop(i){
   }
 }
 
-function getRow(arr, len, i, j, offset=0) {
+function getRow(arr, len, i, j) {
   if (!arr.length) return arr;
   let ret = [];
   const start = (len**2 * i) - 9 + j;
@@ -119,7 +123,7 @@ function getRow(arr, len, i, j, offset=0) {
   return ret;
 }
 
-function getCol(arr, len, offset=0) {
+function getCol(arr, len, offset=0, el=null) {
   if (arr.length < 27) return [];
   let ret = [];
   const maxLen = arr.length < 54 ? 3 : 6;
@@ -130,8 +134,9 @@ function getCol(arr, len, offset=0) {
 
   const start = arr.length + offset - prevBoxCols;
   for (let x = start; x >= 0; x -= 3) { 
-    if (ret.length === 3) x -= 18; 
-    ret.unshift(arr[x][0]) 
+    if (ret.length === 3) x -= 18; //jumping to next box
+    if (el === arr[x][0]) return arr[x][0];
+    ret.unshift(arr[x][0]);
     if (ret.length === maxLen) break;
   };
   return ret;
@@ -141,7 +146,7 @@ function getCol(arr, len, offset=0) {
 //then need the columns from above boxes, if applicable
 
 function getNRow(numbers, arr, len, i, j) {
-  if (numbers.length < 4) return [];
+  if (numbers.length < 4 || numbers.length > 6) return [];
   let nRow = getRow(arr, len, i, j + 3);
   // if (j === 3) nRow = getRow(arr,len,i,j+3);
   return nRow.filter(el => numbers.includes(el)); //flag
@@ -154,14 +159,14 @@ function makeSquares(len = 3) {
     let row;
     let nRow = [];
     for (let j = 0; j < len ** 2; j++) {
-      if (arr.length === 46) debugger;
+      // if (arr.length === 46) debugger;
       if (j % 3 === 0) row = getRow(arr, len, i, j);
       nRow = getNRow(numbers, arr, len, i, j);
       //only relevant since others already used in current box
       const nRowCopy = nRow.splice();
       const col = getCol(arr, numbers.length);
       const nextCols = getCols(numbers, arr, len);
-      let nTried = [];
+      let nTried = []; 
       // let num = j > -1 ? sampleNext(numbers, nRow, nextCols,row, nTried) : sample(numbers, nTried); //flag
       let num = sampleNext(numbers, nRow, nextCols, row, nTried, i, j);
       while (row.includes(num) || col.includes(num)) {
@@ -169,6 +174,8 @@ function makeSquares(len = 3) {
         if (nRowCopy.includes(num)) nRow.push(num);
         // num = j > -1 ? sampleNext(numbers, nRow, nextCols, row, nTried) : sample(numbers, nTried); //flag
         num = sampleNext(numbers, nRow, nextCols, row, nTried, i, j);
+
+        //nTried has all numbers taht arnt in col or row, try to swap the element you must have there based on row up col, will always work?
       }
       arr.push([num, true]);
     }
@@ -180,41 +187,81 @@ let result = makeSquares();
 console.log(result);
 
 console.log('');
-console.log(arr.map(el => el[0]).slice(3*0, 3*1).concat(" ")
-.concat(arr.map(el => el[0]).slice(3*3, 3*4)).concat(" ")
-.concat(arr.map(el => el[0]).slice(3*6, 3*7)).join(" ")
+console.log(result.map(el => el[0]).slice(3*0, 3*1).concat(" ")
+.concat(result.map(el => el[0]).slice(3*3, 3*4)).concat(" ")
+.concat(result.map(el => el[0]).slice(3*6, 3*7)).join(" ")
 );
-console.log(arr.map(el => el[0]).slice(3, 3*2).concat(" ")
-.concat(arr.map(el => el[0]).slice(3*4, 3*5)).concat(" ")
-.concat(arr.map(el => el[0]).slice(3*7, 3*8)).join(" ")
+console.log(result.map(el => el[0]).slice(3, 3*2).concat(" ")
+.concat(result.map(el => el[0]).slice(3*4, 3*5)).concat(" ")
+.concat(result.map(el => el[0]).slice(3*7, 3*8)).join(" ")
 );
-console.log(arr.map(el => el[0]).slice(3*2, 3*3).concat(" ")
-.concat(arr.map(el => el[0]).slice(3*5, 3*6)).concat(" ")
-.concat(arr.map(el => el[0]).slice(3*8, 3*9)).join(" ")
-);
-console.log('');
-console.log(arr.map(el => el[0]).slice(3*9, 3*10).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*12, 3*13)).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*15, 3*16)).join(" ")
-);
-console.log(arr.map(el => el[0]).slice(3*10, 3*11).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*13, 3*14)).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*16, 3*17)).join(" ")
-);
-console.log(arr.map(el => el[0]).slice(3*11, 3*12).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*14, 3*15)).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*17, 3*18)).join(" ")
+console.log(result.map(el => el[0]).slice(3*2, 3*3).concat(" ")
+.concat(result.map(el => el[0]).slice(3*5, 3*6)).concat(" ")
+.concat(result.map(el => el[0]).slice(3*8, 3*9)).join(" ")
 );
 console.log('');
-console.log(arr.map(el => el[0]).slice(3*18, 3*19).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*21, 3*22)).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*24, 3*25)).join(" ")
+console.log(result.map(el => el[0]).slice(3*9, 3*10).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*12, 3*13)).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*15, 3*16)).join(" ")
 );
-console.log(arr.map(el => el[0]).slice(3*19, 3*20).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*22, 3*23)).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*25, 3*26)).join(" ")
+console.log(result.map(el => el[0]).slice(3*10, 3*11).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*13, 3*14)).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*16, 3*17)).join(" ")
 );
-console.log(arr.map(el => el[0]).slice(3*20, 3*21).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*23, 3*24)).concat(" ")
-  .concat(arr.map(el => el[0]).slice(3*26, 3*27)).join(" ")
+console.log(result.map(el => el[0]).slice(3*11, 3*12).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*14, 3*15)).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*17, 3*18)).join(" ")
+);
+console.log('');
+console.log(result.map(el => el[0]).slice(3*18, 3*19).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*21, 3*22)).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*24, 3*25)).join(" ")
+);
+console.log(result.map(el => el[0]).slice(3*19, 3*20).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*22, 3*23)).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*25, 3*26)).join(" ")
+);
+console.log(result.map(el => el[0]).slice(3*20, 3*21).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*23, 3*24)).concat(" ")
+  .concat(result.map(el => el[0]).slice(3*26, 3*27)).join(" ")
+);
+
+console.log('');
+console.log(arr.map(el => el[0]).slice(3 * 0, 3 * 1).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 3, 3 * 4)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 6, 3 * 7)).join(" ")
+);
+console.log(arr.map(el => el[0]).slice(3, 3 * 2).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 4, 3 * 5)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 7, 3 * 8)).join(" ")
+);
+console.log(arr.map(el => el[0]).slice(3 * 2, 3 * 3).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 5, 3 * 6)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 8, 3 * 9)).join(" ")
+);
+console.log('');
+console.log(arr.map(el => el[0]).slice(3 * 9, 3 * 10).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 12, 3 * 13)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 15, 3 * 16)).join(" ")
+);
+console.log(arr.map(el => el[0]).slice(3 * 10, 3 * 11).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 13, 3 * 14)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 16, 3 * 17)).join(" ")
+);
+console.log(arr.map(el => el[0]).slice(3 * 11, 3 * 12).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 14, 3 * 15)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 17, 3 * 18)).join(" ")
+);
+console.log('');
+console.log(arr.map(el => el[0]).slice(3 * 18, 3 * 19).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 21, 3 * 22)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 24, 3 * 25)).join(" ")
+);
+console.log(arr.map(el => el[0]).slice(3 * 19, 3 * 20).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 22, 3 * 23)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 25, 3 * 26)).join(" ")
+);
+console.log(arr.map(el => el[0]).slice(3 * 20, 3 * 21).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 23, 3 * 24)).concat(" ")
+  .concat(arr.map(el => el[0]).slice(3 * 26, 3 * 27)).join(" ")
 );

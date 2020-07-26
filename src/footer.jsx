5cc7a8d2ebe8031319cc987cbe48d84a01198b40
Numbericons <zachjoliver@gmail.com>
2020-07-25 22:09:32 -0700

    Start footer component file

---
+++ src/footer.jsx
@@ -0,0 +1,7 @@
+// <a href="https://github.com/Numbericons/JS_Heads_Up_HoldEm">
+//   <i class="fab fa-github-square"></i>
+// </a>
+//   <a href="https://www.linkedin.com/in/zacharyjoliver/">
+//     <i class="fab fa-linkedin"></i>
+//   </a>
+//   <a class="table-bottom-about-links-name" href="http://zoliver.info/">Zachary Oliver</a>

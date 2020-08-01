7e81e39ec42f0dd50b64d631c052676f15d29dc8
Numbericons <zachjoliver@gmail.com>
2020-08-01 11:39:28 -0700

    Update prompt message

--- src/Game.jsx
+++ src/Game.jsx
@@ -14,7 +14,7 @@ class Game extends React.Component {
 
   render() {
     let val;
-    if (this.loads === 1) val = prompt("Choose difficulty between 1 to 100 for the percentage of squares you want hidden:","50");
+    if (this.loads === 1) val = prompt("Choose a difficulty between 1 to 100 indicating the percentage of squares you want hidden:","50");
     this.loads += 1;
     return <div className='main'>
       <div className='main-head'>

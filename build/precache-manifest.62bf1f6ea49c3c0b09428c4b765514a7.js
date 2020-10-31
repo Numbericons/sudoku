096ae39ae009fb7b15980b80512f135512b7b067
Numbericons <zachjoliver@gmail.com>
2020-10-31 13:19:55 -0700

    Remove buld from gitignore

---
+++ build/precache-manifest.62bf1f6ea49c3c0b09428c4b765514a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a30a413c861494c888068505742a100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6671260f7f2ca7636c16",
+    "url": "/static/css/main.16fec759.chunk.css"
+  },
+  {
+    "revision": "bb7599302b1280d25816",
+    "url": "/static/js/2.1921b306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1921b306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6671260f7f2ca7636c16",
+    "url": "/static/js/main.3a5d74f1.chunk.js"
+  },
+  {
+    "revision": "1bc62db2c40f51cf82e4",
+    "url": "/static/js/runtime-main.b7d0e28b.js"
+  }
+]);
